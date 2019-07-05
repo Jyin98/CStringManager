6ba9457a168eb314c20d906e2a3a7bb584a9b631
Yin <52552321+Jyin98@users.noreply.github.com>
2019-07-04 18:30:46 -0700

    Add files via upload

---
+++ S_Manager.h
@@ -0,0 +1,8 @@
+//S_Manager.h
+void s_clear(char * word);
+char * s_input(void);
+int s_len(char * word);
+void s_print(char * word);
+int s_contain(char * word, char * target);
+char * s_combine(char * word1, char * word2);
+int s_compare(char * word1, char * word2);
